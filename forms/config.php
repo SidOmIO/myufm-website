50d0aede3321a7ea4267b89e94f732513c66f8df
SidOmIO <diksiddiq@gmail.com>
2022-05-19 19:23:57 +0800

    commit on master

--- forms/config.php
+++ forms/config.php
@@ -1,9 +1,17 @@
 <?php
 
-$databaseHost = 'localhost';
-$databaseName = 'myufm';
-$databaseUsername = 'root';
-$databasePassword = '';
+//localhost
+//$databaseHost = 'localhost';
+//$databaseName = 'myufm';
+//$databaseUsername = 'root';
+//$databasePassword = '';
+
+
+//remote database
+$databaseHost = 'remotemysql.com';
+$databaseName = 'g3wpEwN9vW';
+$databaseUsername = 'g3wpEwN9vW';
+$databasePassword = 'R4uMSkNHYw';
 
 $mysqli = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
 
