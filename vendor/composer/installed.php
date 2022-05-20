34390448cbd07477507283cf808cf395751d38ac
SidOmIO <diksiddiq@gmail.com>
2022-05-20 20:55:59 +0800

    First commit cli

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '32b36599ec24e71652353441c6c00410aad2a4b9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '32b36599ec24e71652353441c6c00410aad2a4b9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
